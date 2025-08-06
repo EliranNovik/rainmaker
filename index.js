07bd956024d33fce2e66325162e9b246baffaca8
EliranNovik <eliran.novik@gmail.com>
2025-08-06 16:55:56 +0300

    Refactor index.js to use dynamic lead_source and ref_url parameters from request body

--- index.js
+++ index.js
@@ -13,19 +13,21 @@ app.post('/api/proxy', async (req, res) => {
     phone,
     country,
     message,
-    sid = Date.now().toString()
+    sid = Date.now().toString(),
+    lead_source = '30018', // <-- default only if not provided
+    ref_url = 'https://heritage-based-european-citizenship.lawoffice.org.il/'
   } = req.body;
 
   const url = new URL('https://www.rainmakerqueen.com/hooks/catch/');
   url.searchParams.append('uid', 'fxSOVhSeeRs9');
   url.searchParams.append('sid', sid);
-  url.searchParams.append('lead_source', '30018');
+  url.searchParams.append('lead_source', lead_source);
   url.searchParams.append('name', name || '');
   url.searchParams.append('phone', phone || '');
   url.searchParams.append('email', email || '');
   url.searchParams.append('topic', country || '');
   url.searchParams.append('desc', message || '');
-  url.searchParams.append('ref_url', 'https://heritage-based-european-citizenship.lawoffice.org.il/');
+  url.searchParams.append('ref_url', ref_url);
 
   try {
     const response = await axios.get(url.toString());
